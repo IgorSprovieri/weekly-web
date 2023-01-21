ccb08f2957cd32892e329513cf3935f1ea847c11
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2023-01-21 10:28:31 -0300

    feat: get API colors

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
   loadUser();
+  getAppColors();
   setInitialDate();
   getAndRenderTasks();
 };
@@ -11,26 +12,37 @@ async function loadUser() {
   const userEmail = localStorage.getItem("@Weekly:userEmail");
 
   if (!token || !userId || !userName || !userEmail) {
-    openPage("../../index.html");
-    return;
+    return openPage("../../index.html");
   }
 
-  const result = tryGetUser(token, userEmail);
+  const result = tryGetUser();
 
   if (result.error) {
-    openPage("../../index.html");
-    return;
+    return openPage("../../index.html");
   }
 
   document.getElementById("home-page-username-text").innerText = userName;
 }
 
+async function getAppColors() {
+  const colorsList = await tryGetAppColors();
+  const colorsDataList = document.getElementById("app-colors");
+
+  colorsList.map((item) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = item.hexColor;
+    colorsDataList.appendChild(optionElement);
+  });
+}
+
 function setInitialDate() {
   const dateInput = document.getElementById("home-page-date-input");
-  let prevMonday = new Date();
-  prevMonday.setDate(prevMonday.getDate() - ((prevMonday.getDay() + 6) % 7));
+  let previousMonday = new Date();
+  previousMonday.setDate(
+    previousMonday.getDate() - ((previousMonday.getDay() + 6) % 7)
+  );
 
-  dateInput.value = prevMonday.toISOString().split("T")[0];
+  dateInput.value = previousMonday.toISOString().split("T")[0];
   dateInput.addEventListener("change", () => {
     const dateInput = document.getElementById("home-page-date-input");
     const finalDate = dateFns
@@ -40,21 +52,23 @@ function setInitialDate() {
     document.getElementById("home-page-final-date").value = finalDate;
     getAndRenderTasks();
   });
-  const finalDate = dateFns.addDays(prevMonday, 6).toISOString().split("T")[0];
+
+  const finalDate = dateFns
+    .addDays(previousMonday, 6)
+    .toISOString()
+    .split("T")[0];
   document.getElementById("home-page-final-date").value = finalDate;
 }
 
 async function getAndRenderTasks() {
   const weekDays = [];
-  const token = localStorage.getItem("@Weekly:token");
-  const email = localStorage.getItem("@Weekly:userEmail");
   const initialDate = document.getElementById("home-page-date-input").value;
   const finalDate = document.getElementById("home-page-final-date").value;
   const listsContainer = document.getElementById("home-page-lists-container");
-  document.getElementById("loading-hidden-content").hidden = false;
+  document.getElementById("home-page-loading").style.display = "flex";
   listsContainer.innerHTML = "";
 
-  const result = await tryGetTasks(initialDate, finalDate, token, email);
+  const result = await tryGetTasks(initialDate, finalDate);
   if (result.error) {
     window.alert(result.error);
     return;
@@ -65,21 +79,20 @@ async function getAndRenderTasks() {
     return days[date.getDay()];
   }
 
-  dateAux = new Date(initialDate);
+  let dateAux = new Date(initialDate);
   for (let i = 0; i < 7; i++) {
     document.getElementById("week-day-" + i).innerText = getWeekDay(dateAux);
     weekDays.push(dateAux.toISOString().split("T")[0]);
     dateAux = dateFns.addDays(dateAux, 1);
   }
 
-  const tasks = await tryGetTasks(initialDate, finalDate, token, email);
+  const tasks = await tryGetTasks(initialDate, finalDate);
 
   if (tasks.error) {
-    window.alert("Erro ao carregar as tarefas");
-    return;
+    return window.alert("Erro ao carregar as tarefas");
   }
 
-  document.getElementById("loading-hidden-content").hidden = true;
+  document.getElementById("home-page-loading").style.display = "none";
 
   weekDays.map((day) => {
     const listDayElement = document.createElement("div");
@@ -155,8 +168,180 @@ async function getAndRenderTasks() {
   });
 }
 
-const tryGetTasks = async (initialDate, finalDate, token, email) => {
+function openPage(url) {
+  document
+    .getElementById("body-element")
+    .classList.remove("enterPage-animation");
+  document.getElementById("body-element").classList.add("outPage-animation");
+  setTimeout(() => {
+    window.open(url, "_self");
+  }, 1000);
+}
+
+/*
+####################################################
+onClick functions
+*/
+
+function onClickExit() {
+  localStorage.removeItem("@Weekly:token");
+  localStorage.removeItem("@Weekly:user_id");
+  localStorage.removeItem("@Weekly:userName");
+  localStorage.removeItem("@Weekly:userEmail");
+
+  openPage("../../index.html");
+}
+
+function onClickAddTaskCardButton(date) {
+  document.getElementById("add-task").hidden = false;
+  document.getElementById("add-task-day").value = date;
+  document.getElementById("add-task-initial-Hour").value = "12:00";
+  document.getElementById("add-task-final-Hour").value = "13:00";
+  document.getElementById("add-task-task").value = "";
+  document.getElementById("add-task-description").value = "";
+}
+
+async function onClickAddTask() {
+  document.getElementById("add-task").hidden = true;
+  const day = document.getElementById("add-task-day").value;
+  const initialHour = document.getElementById("add-task-initial-Hour").value;
+  const finalHour = document.getElementById("add-task-final-Hour").value;
+
+  if (finalHour < initialHour) {
+    window.alert("A hora final precisa ser maior que a inicial");
+    return;
+  }
+
+  const bodyData = {
+    task: document.getElementById("add-task-task").value,
+    hexColor: document.getElementById("add-task-color").value,
+    initialDate: `${day}T${initialHour}:00.000Z`,
+    finalDate: `${day}T${finalHour}:00.000Z`,
+    description: document.getElementById("add-task-description").value,
+    checked: document.getElementById("add-task-checked").checked,
+  };
+
+  const result = await tryPostTask(bodyData);
+
+  if (result.error) {
+    return window.alert("Erro ao adicionar a tarefa");
+  }
+
+  getAndRenderTasks();
+}
+
+function onClickBackAddTask() {
+  document.getElementById("add-task").hidden = true;
+}
+
+function onClickPreviousWeek() {
+  addDaysOnCalendar(-7);
+}
+
+function onClickNextWeek() {
+  addDaysOnCalendar(7);
+}
+
+function addDaysOnCalendar(days) {
+  const dateInput = document.getElementById("home-page-date-input");
+  const startDate = dateFns
+    .addDays(dateInput.value, days)
+    .toISOString()
+    .split("T")[0];
+  const finalDate = dateFns.addDays(startDate, 6).toISOString().split("T")[0];
+
+  dateInput.value = startDate;
+  document.getElementById("home-page-final-date").value = finalDate;
+
+  getAndRenderTasks();
+}
+
+async function onClickCheckTask(task) {
+  const bodyData = {
+    checked: !task.checked,
+  };
+
+  const result = await tryPutTask(bodyData, task._id);
+
+  if (result.error) {
+    return window.alert("Não foi possível concluir a tarefa");
+  }
+
+  task.checked = !task.checked;
+
+  const firstElement = document.getElementById(task._id).firstElementChild;
+
+  if (task.checked == true) {
+    firstElement.innerHTML = `<s>${task.task}</s>`;
+  } else {
+    firstElement.innerHTML = `${task.task}`;
+  }
+}
+
+function onClickEditTask(task) {
+  window.alert("Edit Task: " + task._id);
+}
+
+async function onClickDeleteTask(task) {
+  const result = await tryDeleteTask(task._id);
+
+  if (result.error) {
+    return window.alert("Não foi possível deletar a tarefa");
+  }
+
+  document.getElementById(task._id).remove();
+}
+
+/*
+###########################################################
+API Requests
+*/
+
+const tryGetAppColors = async () => {
   try {
+    const result = await fetch("https://weekly.herokuapp.com/colors");
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    return { error };
+  }
+};
+
+// Auth Requests
+const getHeader = () => {
+  const token = localStorage.getItem("@Weekly:token");
+  const email = localStorage.getItem("@Weekly:userEmail");
+
+  const data = new Object();
+  data.token = token;
+  data.email = email;
+
+  return data;
+};
+
+const tryGetUser = async () => {
+  try {
+    const { token, email } = getHeader();
+
+    const result = await fetch("https://weekly.herokuapp.com/user", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+        email: email,
+      },
+    });
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    return { error };
+  }
+};
+
+const tryGetTasks = async (initialDate, finalDate) => {
+  try {
+    const { token, email } = getHeader();
+
     const result = await fetch(
       `https://weekly.herokuapp.com/task?initialDate=${initialDate}T00%3A00%3A00.000Z&finalDate=${finalDate}T23%3A59%3A59.000Z`,
       {
@@ -175,82 +360,9 @@ const tryGetTasks = async (initialDate, finalDate, token, email) => {
   }
 };
 
-const tryGetUser = async (token, email) => {
+const tryPostTask = async (bodyData) => {
   try {
-    const result = await fetch("https://weekly.herokuapp.com/user", {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
-        email: email,
-      },
-    });
-    const data = await result.json();
-    return data;
-  } catch (error) {
-    return { error };
-  }
-};
-
-function onClickExit() {
-  localStorage.removeItem("@Weekly:token");
-  localStorage.removeItem("@Weekly:user_id");
-  localStorage.removeItem("@Weekly:userName");
-  localStorage.removeItem("@Weekly:userEmail");
-
-  openPage("../../index.html");
-}
-
-function openPage(url) {
-  document
-    .getElementById("body-element")
-    .classList.remove("enterPage-animation");
-  document.getElementById("body-element").classList.add("outPage-animation");
-  setTimeout(() => {
-    window.open(url, "_self");
-  }, 1000);
-}
-
-function onClickAddTaskCardButton(date) {
-  document.getElementById("add-task").hidden = false;
-  document.getElementById("add-task-day").value = date;
-  document.getElementById("add-task-initial-Hour").value = "12:00";
-  document.getElementById("add-task-final-Hour").value = "13:00";
-}
-
-async function onClickAddTask() {
-  document.getElementById("add-task").hidden = true;
-  const day = document.getElementById("add-task-day").value;
-  const initialHour = document.getElementById("add-task-initial-Hour").value;
-  const finalHour = document.getElementById("add-task-final-Hour").value;
-  const token = localStorage.getItem("@Weekly:token");
-  const email = localStorage.getItem("@Weekly:userEmail");
-
-  if (finalHour < initialHour) {
-    window.alert("A hora final precisa ser maior que a inicial");
-    return;
-  }
-
-  const result = await tryPostTask(token, email, day, initialHour, finalHour);
-
-  if (result.error) {
-    window.alert("Erro ao adicionar a tarefa: " + result.error);
-    return;
-  }
-
-  getAndRenderTasks();
-}
-
-const tryPostTask = async (token, email, day, initialHour, finalHour) => {
-  try {
-    const bodyData = {
-      task: document.getElementById("add-task-task").value,
-      hexColor: document.getElementById("add-task-color").value,
-      initialDate: `${day}T${initialHour}:00.000Z`,
-      finalDate: `${day}T${finalHour}:00.000Z`,
-      description: document.getElementById("add-task-description").value,
-      checked: document.getElementById("add-task-checked").checked,
-    };
+    const { token, email } = getHeader();
 
     const result = await fetch("https://weekly.herokuapp.com/task", {
       method: "post",
@@ -271,104 +383,47 @@ const tryPostTask = async (token, email, day, initialHour, finalHour) => {
   }
 };
 
-function onClickBackAddTask() {
-  document.getElementById("add-task").hidden = true;
-}
-
-function onClickPreviousWeek() {
-  const dateInput = document.getElementById("home-page-date-input");
-  const startDate = dateFns
-    .addDays(dateInput.value, -7)
-    .toISOString()
-    .split("T")[0];
-  const finalDate = dateFns.addDays(startDate, 6).toISOString().split("T")[0];
-
-  dateInput.value = startDate;
-  document.getElementById("home-page-final-date").value = finalDate;
-
-  getAndRenderTasks();
-}
-
-function onClickNextWeek() {
-  const dateInput = document.getElementById("home-page-date-input");
-  const startDate = dateFns
-    .addDays(dateInput.value, 7)
-    .toISOString()
-    .split("T")[0];
-  const finalDate = dateFns.addDays(startDate, 6).toISOString().split("T")[0];
-
-  dateInput.value = startDate;
-  document.getElementById("home-page-final-date").value = finalDate;
-
-  getAndRenderTasks();
-}
-
-async function onClickCheckTask(task) {
+const tryPutTask = async (bodyData, taskId) => {
   try {
-    const token = localStorage.getItem("@Weekly:token");
-    const email = localStorage.getItem("@Weekly:userEmail");
-    task.checked = !task.checked;
+    const { token, email } = getHeader();
 
-    const result = await fetch(
-      `https://weekly.herokuapp.com/task/${task._id}`,
-      {
-        method: "PUT",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-          Authorization: "Bearer " + token,
-          email: email,
-        },
-        body: {
-          checked: task.checked.toString(),
-        },
-      }
-    );
-    await result.json();
-
-    const taskText = document.getElementById(task._id).firstElementChild;
-    if (task.checked == true) {
-      taskText.innerHTML = `<h6><s>${task.task}</s><h6>`;
-    } else {
-      taskText.innerHTML = `<h6>${task.task}<h6>`;
-    }
-
-    return;
+    const result = await fetch(`https://weekly.herokuapp.com/task/${taskId}`, {
+      method: "put",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+        email: email,
+      },
+      body: JSON.stringify(bodyData),
+    });
+    const data = await result.json();
+    return data;
   } catch (error) {
-    window.alert("Erro ao concluir tarefa: " + error);
-    return;
+    return { error };
   }
-}
+};
 
-function onClickEditTask(task) {
-  window.alert("Edit Task: " + task._id);
-}
-
-async function onClickDeleteTask(task) {
+const tryDeleteTask = async (taskId) => {
   try {
-    const token = localStorage.getItem("@Weekly:token");
-    const email = localStorage.getItem("@Weekly:userEmail");
+    const { token, email } = getHeader();
 
-    const result = await fetch(
-      `https://weekly.herokuapp.com/task/${task._id}`,
-      {
-        method: "delete",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + token,
-          email: email,
-        },
-      }
-    );
-    await result.json();
-    document.getElementById(task._id).remove();
-    return;
+    const result = await fetch(`https://weekly.herokuapp.com/task/${taskId}`, {
+      method: "delete",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+        email: email,
+      },
+    });
+    const data = await result.json();
+    return data;
   } catch (error) {
-    window.alert("Erro ao deletar tarefa");
-    return;
+    return { error };
   }
-}
+};
